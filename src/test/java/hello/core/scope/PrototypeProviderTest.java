57ee8fceed37a767bd8dec975f2e33f22d37549b
garve32 <garve32@gmail.com>
2023-03-12 16:24:54 +0900

    Spring basic 0312

---
+++ src/test/java/hello/core/scope/PrototypeProviderTest.java
@@ -0,0 +1,14 @@
+package hello.core.scope;
+
+import org.junit.jupiter.api.Test;
+
+public class PrototypeProviderTest {
+    @Test
+    void providerTest() {
+
+    }
+
+    static class ClientBean {
+
+    }
+}
